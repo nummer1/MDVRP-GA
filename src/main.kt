1e51eecf560631aeee67a1377c054b9a0c819d1d
nummer1 <AnonymNN2@gmail.com>
2019-02-11 00:12:15 +0100

    removed todo

--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,3 @@
-// TODO: add more Randomness to route crossover and mutation
-// TODO: when valid solution found - save solution (if better than last valid solution)
-// TODO: fix saving of files when exiting program
-// https://towardsdatascience.com/introduction-to-genetic-algorithms-including-example-code-e396e98d8bf3
-// https://pdfs.semanticscholar.org/c8ec/052d0266678e4deb20ac7af1014f9d7a27b4.pdf
-// see wikipedia genetic algorithm and edge recombination
-
-// https://www.mathworks.com/help/gads/how-the-genetic-algorithm-works.html
-
 
 fun main(args: Array<String>) {
 
